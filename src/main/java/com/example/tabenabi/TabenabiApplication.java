8db3b570db5f7d086241580165adcb0f3222720b
kazuki-shimura <sim202025@icloud.com>
2021-11-07 22:52:58 +0900

    CreateProject

---
+++ src/main/java/com/example/tabenabi/TabenabiApplication.java
@@ -0,0 +1,14 @@
+package com.example.tabenabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabenabiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("たべナビStart");
+		SpringApplication.run(TabenabiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tabenabi/TabenabiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tabenabi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabenabiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
