287a9619cf94f8021b6139b52026d040b97785b6
kazuki-shimura <sim202025@icloud.com>
2021-11-08 00:09:41 +0900

    Welcomeページの作成

--- src/main/java/com/example/tabenabi/controller/StoreController.java
+++ src/main/java/com/example/tabenabi/controller/StoreController.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StoreController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "welcome";
+    }
+
     @GetMapping("/locations")
     public String locations() {
         return "Locations";
